ef3998ae9c4ce1492f319d48b9fe9df044ab3559
sarahp27 <sarah.patel@xloopdigital.com>
2023-11-08 14:19:21 +0500

    props added for reusability of code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Footer from './components/Footer';
 import YellowBanner from './components/YellowBanner';
 
 function App() {
+
+  const head=('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad  minim veniam. ')
+  const yellowpara = ('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.')
   return (
     <div className="App">
-      <YellowBanner/>
+      <YellowBanner havePara={true} heading={head} p={yellowpara}  />
       <Footer />
     </div>
   );
--- src/components/YellowBanner.jsx
+++ src/components/YellowBanner.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import styles from "./YellowBanner.module.css";
 
-export default function YellowBanner() {
-  return (
-    <div className={styles.bannerContent}>
-       <h2>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-           eiusmod tempor incididunt ut labore.
-         </h2>
-         <p className={styles.para}>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-           minim veniam.
-         </p>
-       </div>
-  )
+export default function YellowBanner({ havePara, p, heading }) {
+
+  
+  if (havePara) {
+    return (
+      <div className={styles.bannerContent}>
+         <h2>
+          {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore. */}
+          {p}
+        </h2>
+        <p className={styles.para}>
+          {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam. */}
+          {heading}
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.bannerContent}>
+        <h2>
+          {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore. */}
+
+          {heading}
+        </h2>
+        
+      </div>
+    );
+  }
 }
